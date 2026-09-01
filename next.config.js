0a6ad3d549f36b4c1c49bd87b7ef309a6a3cb570
Pedro Quispe <pqctec@gmail.com>
2026-09-01 08:02:28 -0500

    Actualizando codigo y configuracion para GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  // Configuración necesaria si usas componentes de imágenes (next/image) con exportación estática
+  images: {
+    unoptimized: true,
+  },
+  // Reemplaza 'Cherry-Bloom-Studio-Web' con el nombre exacto de tu repositorio en GitHub si fuera necesario, 
+  // esto asegura que las rutas de tus archivos e imágenes carguen bien en GitHub Pages:
+  basePath: '/Cherry-Bloom-Studio-Web',
+  assetPrefix: '/Cherry-Bloom-Studio-Web/',
 }
 
 module.exports = nextConfig
